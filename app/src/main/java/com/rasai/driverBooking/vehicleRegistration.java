7e0e4a480f662215f18bff8a759cbb579a9456e1
miaaaahmed97 <51957914+miaaaahmed97@users.noreply.github.com>
2019-06-26 01:30:21 +0500

    vehicle xml

--- app/src/main/java/com/rasai/driverBooking/vehicleRegistration.java
+++ app/src/main/java/com/rasai/driverBooking/vehicleRegistration.java
@@ -3,6 +3,8 @@ package com.rasai.driverBooking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class vehicleRegistration extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class vehicleRegistration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vehicle_registration);
+
+        Spinner manufacturerSpinner = (Spinner) findViewById(R.id.manufacturerSpinner);
+        ArrayAdapter<CharSequence> manufacturerAdapter = ArrayAdapter.createFromResource(this, R.array.manufacturer_array, android.R.layout.simple_spinner_item);
+        manufacturerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        manufacturerSpinner.setAdapter(manufacturerAdapter);
+
+        Spinner seatsSpinner = (Spinner) findViewById(R.id.seatsSpinner);
+        ArrayAdapter<CharSequence> seatsAdapter = ArrayAdapter.createFromResource(this,
+        R.array.seats_array, android.R.layout.simple_spinner_item);
+        seatsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        seatsSpinner.setAdapter(seatsAdapter);
+
+
+
     }
 }
