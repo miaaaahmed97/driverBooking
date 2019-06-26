9bb038f53986f6a36ae22c865e3ee3359c452a37
miaaaahmed97 <51957914+miaaaahmed97@users.noreply.github.com>
2019-06-26 11:26:15 +0500

    starting driver home

--- app/src/main/java/com/rasai/driverBooking/DriverHome.java
+++ app/src/main/java/com/rasai/driverBooking/DriverHome.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DriverHome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_home);
+
+        DatabaseReference mRef = FirebaseDatabase.getInstance().getReference().child("Trips");
     }
 }
