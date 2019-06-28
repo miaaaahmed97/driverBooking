3115870620a3dcf0af75279487c81a4c8982e0c4
miaaaahmed97 <51957914+miaaaahmed97@users.noreply.github.com>
2019-06-28 15:33:11 +0500

    Change Budget Done

---
+++ app/src/main/java/com/rasai/driverBooking/TripTabsActivity.java
@@ -0,0 +1,14 @@
+package com.rasai.driverBooking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TripTabsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_tabs);
+    }
+}
